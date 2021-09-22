9862e06e1786211f541477895df3a697d4d276cf
Nina Sophie Rijntjes <90196169+n1en@users.noreply.github.com>
2021-09-22 09:26:04 +0200

    hi, ik heb verder gewerkt aan alles

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-// JavaScript Document
+function myFunction() {
+    var menuknop = document.getElementById("myLinks");
+    if (menuknop.style.display === "block") {
+      menuknop.style.display = "none";
+    } else {
+      menuknop.style.display = "block";
+    }
+  }
+
